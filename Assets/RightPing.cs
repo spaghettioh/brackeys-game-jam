a1475ac4cc2287e8cc77ad5cd76d90d64b88e443
Eric Mofield <ericmofield@gmail.com>
2019-02-17 23:42:25 -0500

    Add light pings.

---
+++ Assets/RightPing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RightPing : MonoBehaviour {
+    public UnityEvent pingOn;
+    public UnityEvent pingOff;
+    public bool canPing = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButtonDown("RightPing") && canPing)
+        {
+            canPing = false; 
+            pingOn.Invoke();
+            StartCoroutine(DisableLight());
+        }
+    }
+
+    private IEnumerator DisableLight()
+    {
+        yield return new WaitForSeconds(1);
+        pingOff.Invoke();
+        canPing = true;
+    }
+}
