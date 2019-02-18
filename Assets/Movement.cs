fe51a847e353c958cb54805511aa08408675b92e
Eric Mofield <ericmofield@gmail.com>
2019-02-17 22:52:11 -0500

    Add a test scene for controller movement.

---
+++ Assets/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D body;
+    public float moveSpeed;
+     
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, Input.GetAxis("Vertical") * moveSpeed); 
+    }
+}
