25202d67d825901cee60de91875a4a073b10d68b
Eric Mofield <ericmofield@gmail.com>
2019-02-18 01:30:39 -0500

    Setup UI to show win condition.

---
+++ Assets/Scripts/BlueGoal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BlueGoal : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+	// Use this for initialization
+	void Start()
+    {
+        text.text = "";
+	}
+	
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Blue")
+        {
+            text.text = "Blue is in!";
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        text.text = "";
+    }
+}
