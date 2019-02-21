5a21e1fd4696d8386dd406f04c82b507288225be
Eric Mofield <ericmofield@gmail.com>
2019-02-20 23:31:54 -0500

    Fix all the things.
    Sprite masking, prefab brushes with path settings, etc. Level feels real now....

---
+++ Assets/Scripts/PingScaler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingScaler : MonoBehaviour
+{
+    public float timing = 2;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Kill());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 scale = transform.localScale;
+
+        scale.x += .1f;
+        scale.y += .1f;
+
+        transform.localScale = scale;
+    }
+
+    IEnumerator Kill()
+    {
+        yield return new WaitForSeconds(timing);
+        Destroy(gameObject);
+    }
+}
