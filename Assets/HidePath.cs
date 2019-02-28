7275952e1364ae9903af3bd0257154a1e6150577
Eric Mofield <ericmofield@gmail.com>
2019-02-28 01:08:00 -0500

    Update path prefabs with color and hide on play.

---
+++ Assets/HidePath.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidePath : MonoBehaviour
+{
+    public SpriteRenderer sprite;
+
+	void Start () {
+        sprite.color = new Color(0, 0, 0, 1);
+	}
+}
